dee044c2b054cebcb5d9e8481bfb9275e27c7c10
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2024-07-25 15:56:59 +0530

    Added:Taking input from console for Address Book

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book");
+        Contact obj = new Contact();
+        Scanner scanner = new Scanner(System.in);
     }
 }
